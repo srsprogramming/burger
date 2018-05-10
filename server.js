64961cdf4ae92856c5ffcdf537e3267fe1927150
Steven Shirley <stevenshirley@Stevens-MBP.router>
2018-05-10 17:39:54 -0500

    final commit; cleaned up server

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
